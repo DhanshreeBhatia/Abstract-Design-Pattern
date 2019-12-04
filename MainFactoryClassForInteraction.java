96ededfca6e95e66cb2f1f0ea5a6e2d304ed910f
Dhanshree Bhatia <33285607+DhanshreeBhatia@users.noreply.github.com>
2019-12-04 20:44:06 +0530

    Delete MainFactoryClassForInteraction.java

--- MainFactoryClassForInteraction.java
+++
@@ -1,9 +0,0 @@
-package com.main.java;
-
-public class MainFactoryClassForInteraction {
-
-	
-	public static AbstractSuperClass getComputer(AbstractMainFactoryClass factory){
-		return factory.createComputer();
-	}
-}
